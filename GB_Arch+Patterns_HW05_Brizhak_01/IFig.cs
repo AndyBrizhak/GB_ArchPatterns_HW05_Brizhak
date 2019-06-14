e5a54e7b323d9c65b7c5f8dfd030b7738050e37e
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-14 20:19:03 +0800

    +interface IFig

---
+++ GB_Arch+Patterns_HW05_Brizhak_01/IFig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW05_Brizhak_01
+{
+    interface IFig
+    {
+        void Draw(Graphics g, int x, int y, int width, int height);
+    }
+}
