96e5af242d834d1d7ec5a1c0bcb33949308a529a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-16 09:52:18 +0800

    +class Math : IMath +class ProxyMath

--- GB_Arch+Pattrens_HW05_Brizhak_02/Math.cs
+++ GB_Arch+Pattrens_HW05_Brizhak_02/Math.cs
@@ -11,24 +11,59 @@ namespace GB_Arch_Pattrens_HW05_Brizhak_02
     /// </summary>
     class Math : IMath
     {
+        /// <summary>
+        /// Метод возвращает сумму чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Add(int x, int y)
         {
             return x + y;
         }
 
+        /// <summary>
+        /// Метод возвращает разность чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Div(int x, int y)
         {
             return x - y;
         }
 
+        /// <summary>
+        /// Метод возвращает результат умножения чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Mul(int x, int y)
         {
             return x * y;
         }
 
+        /// <summary>
+        /// Метод возвращает результат деления чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Sub(int x, int y)
         {
             return x / y;
+        }
+
+        /// <summary>
+        /// Метод возвращает медиану двух чисел
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int MedTwoNum(int x, int y)
+        {
+            return (x+y)/2 ;
         }
     }
 }
---
+++ GB_Arch+Pattrens_HW05_Brizhak_02/ProxyMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Pattrens_HW05_Brizhak_02
+{
+    class ProxyMath
+    {
+        Math math = new Math();
+
+        public int Add(int x, int y)
+        {
+            return math.Add(x, y);
+        }
+
+        public int Sub(int x, int y)
+        {
+            return math.Sub(x, y);
+        }
+
+        public int Mul(int x, int y)
+        {
+            return math.Mul(x, y);
+        }
+
+        public int Div(int x, int y)
+        {
+            return math.Div(x, y);
+        }
+
+    }
+}
