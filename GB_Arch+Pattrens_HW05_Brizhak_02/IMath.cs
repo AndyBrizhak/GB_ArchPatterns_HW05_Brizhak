ae68576f5cfd8183ac19d731df8f0196d9d223f8
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-16 08:23:06 +0800

    +

--- GB_Arch+Pattrens_HW05_Brizhak_02/IMath.cs
+++ GB_Arch+Pattrens_HW05_Brizhak_02/IMath.cs
@@ -11,7 +11,7 @@ namespace GB_Arch_Pattrens_HW05_Brizhak_02
     /// </summary>
     public interface IMath
     {
-        int add(int x, int y);
+        int Add(int x, int y);
         int Sub(int x, int y);
         int Mul(int x, int y);
         int Div(int x, int y);
