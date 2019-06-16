e4d66f6c2186a73db20ce117f06f6eef0883db65
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-16 09:26:58 +0800

    +class Math : IMath

---
+++ GB_Arch+Pattrens_HW05_Brizhak_02/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Pattrens_HW05_Brizhak_02
+{
+    /// <summary>
+    /// Исходный класс Math
+    /// </summary>
+    class Math : IMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Div(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
