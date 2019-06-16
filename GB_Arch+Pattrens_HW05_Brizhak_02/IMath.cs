0610f2bed93a04e83e190b5ec7c774f5b5e7b0c2
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-16 08:19:43 +0800

    +public interface IMath

---
+++ GB_Arch+Pattrens_HW05_Brizhak_02/IMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Pattrens_HW05_Brizhak_02
+{
+    /// <summary>
+    /// Интерфейс для реализации методов класса Math
+    /// </summary>
+    public interface IMath
+    {
+        int add(int x, int y);
+        int Sub(int x, int y);
+        int Mul(int x, int y);
+        int Div(int x, int y);
+    }
+}
--- GB_Arch+Pattrens_HW05_Brizhak_02/Program.cs
+++ GB_Arch+Pattrens_HW05_Brizhak_02/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// ДЗ к уроку 5 по курсу Архитектуры и шаблоны проектирования Брижак Андрей
+//2. Реализовать «заместителя» при работе с классом Math, ограничив его четырьмя функциями:
+//+, -, *, / .
 
 
 namespace GB_Arch_Pattrens_HW05_Brizhak_02
